1c26b12bc65cacdecaf3ec0ed018be1cc6e5d611
kunsam <kunsam624@icloud.com>
2019-11-01 10:48:27 +0800

    add datautil

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@
 
 export * from './typings/index'
 export { default as Program } from './program'
+export { default as DataTransfromUtil } from './data_util'
 export { default as SourceFileUtil } from './sourefile_util'
 export { default as TsSyntaxResolveUtil } from './syntax_util'
